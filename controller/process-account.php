bcd93423620c224dab6efb36c38d8f33bc8c3dae
Gilmar Pereira <gilmarpereira1212@hotmail.com>
2022-11-10 22:10:12 -0300

    create validation of sign in

--- controller/process-account.php
+++ controller/process-account.php
@@ -2,25 +2,19 @@
     include("../model/connect-db.php");
     class Process_account extends Connect_db {
     public $fetch;
-    public function throwsError(){
-        try{
-            throw new Exception("Error, usuário e senha já existem");
-            $register["login"] == $_POST["login"];
-            $register["senha"] == $_POST["senha"];
-           }catch(Exception $e){
-            echo $e->getMessage();
-           }
-    }
-    public function process(){
-
+    //Validate errors of equals login and password and empty input
+    public function throwsErrorCreation(){
+        
         $this->fetch = mysqli_query(Process_account::connect(),"SELECT * FROM cadastro");
         while($register = mysqli_fetch_assoc($this->fetch)){
-            Process_account::throwsError();
-          
-           
+            if( $register["login"] == $_POST["login"]&& $register["senha"] == $_POST["senha"] || $register["login"] == $_POST["login"] || $register["senha"] == $_POST["senha"])
+            throw new Exception("Error, usuário e senha já existem");
+    }
     
-        }
-    } 
+    if(empty($login) && empty($senha)){
+        throw new Exception("Error, o cadastro não pode possuir o valor vazio, por favor preencha corretamente");
+    }
+    }
     }
     
     
--- model/create-account.php
+++ model/create-account.php
@@ -1,39 +1,31 @@
 <?php
-  session_start(); 
+ 
   
    include_once("../controller/process-account.php");
    include_once("connect-db.php");
     class createAccount extends Connect_db{
         private $login;
         private $senha;
-        public function Create(String $login, String $senha){
-            
+        public function Create(String $login, String $senha){            
             $this->login = $login;
             $this->senha = $senha;
-            var_dump(createAccount::connect());
-               
-                    
-                
-               
-           
-                if(empty($login) && empty($senha)){
-                    throw new Exception("Error, o cadastro não pode possuir o valor vazio, por favor preencha corretamente");
-                }else{
+
                     $insert = "INSERT INTO cadastro (`login`,`senha`) VALUES ('".$login."','".$senha."')";
                     mysqli_query(createAccount::connect(), $insert);
                     echo"Cadastro realizado com sucesso";
-
-                }
-            }
-           
+            }    
     }
    
-    
     $validate = new Process_account();
-    // echo $validate->process();
-    if(!$validate->process()){
-        $create = new createAccount();
-        $create ->Create($_POST["login"],$_POST["senha"]);
-    }
-    session_destroy();
+    $validate->throwsError($_POST['login'],$_POST['senha']);
+
+    $create = new createAccount();
+    $create->Create($_POST['login'],$_POST['senha']);
+   
+    
+
+    
+        
+        
+  
 ?>
