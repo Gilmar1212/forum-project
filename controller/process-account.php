d13bc54423de90e5ca1eefa74eed0552b456926b
Gilmar Pereira <gilmarpereira1212@hotmail.com>
2023-01-29 21:01:19 -0300

    subatualizando tratamento de erros conforme display erros

--- controller/process-account.php
+++ controller/process-account.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors', 1);
     include("../../connection/connect-db.php");
     class Process_account extends Connect_db {
     public $fetch;
@@ -6,23 +7,30 @@
     public function throwsErrorCreation($login,$senha,$repete_senha){
         $this->fetch = mysqli_query(Process_account::connect(),"SELECT * FROM cadastro");
         while($register = mysqli_fetch_assoc($this->fetch)){
-     if( $register["login"] == $login){
-        throw new Exception("Error, Login já existe");
-    }            
-    if($senha !== $repete_senha){
-        throw new Exception("Error, Senha e repete senha não são iguais");
-    }
-    if(empty($login) && empty($senha) && empty($repete_senha)){
-        throw new Exception("Error, o cadastro não pode possuir o valor vazio, por favor preencha corretamente");
-    }       
-    if( $register["login"] == $login){
-        echo "Error, Login já existe";
-    }            
-    if($senha !== $repete_senha){
-        echo "Error, Senha e repete senha não são iguais";
-    }
-    if(empty($login) && empty($senha) && empty($repete_senha)){
-        echo "Error, o cadastro não pode possuir o valor vazio, por favor preencha corretamente";
+    if(ini_set('display_errors', 1)){
+        if( $register["login"] == $login){
+            echo "Error, Login já existe";
+            break;
+        }            
+        if($senha !== $repete_senha){
+            echo "Error, Senha e repete senha não são iguais";
+            break;
+        }
+        if(empty($login) && empty($senha) && empty($repete_senha)){
+            echo "Error, o cadastro não pode possuir o valor vazio, por favor preencha corretamente";
+            break;
+        } 
+    } 
+    else{
+        if( $register["login"] == $login){
+            throw new Exception("Error, Login já existe");
+        }            
+        if($senha !== $repete_senha){
+            throw new Exception("Error, Senha e repete senha não são iguais");
+        }
+        if(empty($login) && empty($senha) && empty($repete_senha)){
+            throw new Exception("Error, o cadastro não pode possuir o valor vazio, por favor preencha corretamente");
+        }       
     }       
     }
     }
